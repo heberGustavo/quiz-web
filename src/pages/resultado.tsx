d73098c5eed70f931e86a2c82313fb3ecb179edb
heberGustavo <heber.gbarbosa@hotmail.com>
2024-09-12 09:14:31 -0300

    Customization in Resposta and Estatistica component

--- src/pages/resultado.tsx
+++ src/pages/resultado.tsx
@@ -1,20 +1,25 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useRouter } from "next/router";
 import styles from "../styles/Resultado.module.css";
+import Estatistica from "../../components/Estatistica";
+import Botao from "../../components/Botao";
 
-export default function resultado(){
+export default function resultado() {
     const router = useRouter();
 
     const total = +`${router.query.total}`
     const certas = +`${router.query.certas}`
     const percentual = Math.round((certas / total) * 100);
 
-    return(
+    return (
         <div className={styles.resultado}>
             <h1>Resultado Final</h1>
-            <div>{total}</div>
-            <div>{certas}</div>
-            <div>{`${percentual}%`}</div>
+            <div style={{ display: "flex" }}>
+                <Estatistica texto="Perguntas" valor={total} />
+                <Estatistica texto="Certas" valor={certas} corFundo="#9CD2A4" />
+                <Estatistica texto="Percentual" valor={`${percentual}%`} corFundo="#DE6A33" />
+            </div>
+            <Botao href="/" texto="Tentar novamente" />
         </div>
     )
 }
